b5cef73abdb1ca6c7588db5361516ef0acdcb9bb
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-07-14 22:57:11 +0200

    feat: add personal project items in portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -28,20 +28,39 @@ const Portfolio: NextPage = () => {
           </div>
           <div className="row">
             {/* Portfolio Item */}
-            <div className="portfolio-item padd-15" data-category="web-design">
+            <div className="portfolio-item padd-15" data-category="cross-platform-app">
               <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/1.jpg" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>Web Design</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
+                <a href="https://vocaltrainer.netlify.app" target="_blank" rel="noopener noreferrer">
+                  <div className="portfolio-img">
+                    <img src="/imgs/portfolio/VocalTrainer.jpeg" alt="portfolio" />
                   </div>
-                </div>
+                  <div className="portfolio-info">
+                      <h4>Vocal Trainer</h4>
+                      <div className="icon">
+                        <i className="fa fa-chevron-right"></i>
+                      </div>
+                  </div>
+                </a>
               </div>
             </div>
-            {/* Portfolio Item End*/}
+            {/* Portfolio Item End */}
+            {/* Portfolio Item */}
+            <div className="portfolio-item padd-15" data-category="cross-platform-app">
+              <div className="portfolio-item-inner shadow-dark">
+                <a href="https://sabaothctdocs.netlify.app" target="_blank" rel="noopener noreferrer">
+                  <div className="portfolio-img">
+                    <img src="/imgs/portfolio/SabaothCTDocs.jpeg" alt="portfolio" />
+                  </div>
+                  <div className="portfolio-info">
+                      <h4>Documentation APS Sabaoth Catania</h4>
+                      <div className="icon">
+                        <i className="fa fa-chevron-right"></i>
+                      </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            {/* Portfolio Item End */}
           </div>
         </div>
       </section>
