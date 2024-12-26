d9ecf927c6a5b5f80cac6bb59bc3515d8543bf95
Simone Guarnuccio <simoneguarnuccio@creationdose.com>
2024-12-26 18:46:16 +0100

    feat: adjust image dimensions for GS-Photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,12 @@ export default async function Home() {
               src="/imgs/gs-photo.png"
               alt="gscripts"
               className="shadow-dark"
-              width={300}
-              height={300}
+              width={280}
+              height={280}
             />
             <h1>Simone Guarnuccio</h1>
             <p>Frontend Developer</p>
             <div className="social-links">
-              {/* <a href="https://twitter.com/" target="_blank">
-                <i className="fa fa-twitter" />
-              </a> */}
               <a href="https://www.linkedin.com/authwall?trk=gf&trkInfo=AQGx5HKBKsr_TQAAAZBj64m4B-v-SF6jUFHCW1zj2Jp0chYKE-nSMUpwmBs-U1YKTNRb7Zgx6Z_okklU2MrdF3nuimoWHZjELM9rI5HSDfVh1yQShbYYsWPUO3vKApgv6-fa5Y4=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fsimone-guarnuccio-7779a4166%3Futm_source%3Dshare%26utm_campaign%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dandroid_app" target="_blank">
                 <i className="fa fa-linkedin" />
               </a>
